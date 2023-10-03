83a1c53b26b42e1da28967544b69c191c8ddb1e5
rinogabriel <gabrielpandelaki7@gmail.com>
2023-10-03 09:18:22 +0800

    haha
    hehe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./mark.jpg";
+import "./App.css";
+import  React, {useState} from 'react';
 
 function App() {
+  const [isiCircle, setIsiCircle] = useState(true);
+
+  const toggleShape  = () => {setIsiCircle(!isiCircle);
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="fotopalingatas">
+          <img src={logo} className={isiCircle? 'circle': 'square' } alt="logo" />
+          <div className="tulisandibawahfoto">
+            <ul>
+              <li>Gender: Male</li>
+              <li>Carer: Mahasiswa</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="tulisandisampingfoto">
+          <div>
+            <h2>Rino Gabriel Pandelaki</h2>
+            <hr></hr>
+          </div>
+          <div className="tulisandibawahgaris">
+            <ul>
+              <li>Angkatan : 2022</li>
+              <li>Major : Informatics</li>
+              <li>Jalur Sukses : Startup</li>
+              <li>Hobby : Gaming</li>
+            </ul>
+          </div>
+        </div>
       </header>
+
+      <div className="right-bottom">
+        <button className='button' onClick={toggleShape}>Ubah</button>
+      </div>
+
+      <div className="deskripsi">
+        <hr></hr>
+        <p>Riwayat Organisasi : </p>
+        <ul>
+         <li> Ga ada</li>
+
+        </ul>
+        </div>
+
     </div>
   );
 }
